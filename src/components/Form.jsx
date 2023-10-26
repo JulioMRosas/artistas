1fba0f45579f90598fed196bf26c8f7190b47506
JulioMRosas <al21760482@ite.edu.mx>
2023-10-25 18:06:26 -0700

    minimal design on form and header components

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -29,26 +29,28 @@ const Form = () => {
   }
 
   return <>
-    <form className='w-[90%] mt-6 space-y-5'>
-        <div className='flex flex-col space-y-3'>
-            <label htmlFor="name" className='font-semibold'>Nombre del artista</label>
-            <input type="text" placeholder='ej. Hannah Montana' id='name' className='p-2 rounded-md bg-[#470d0db4]' value={name} onChange={e => setName(e.target.value)}/>
-        </div>
-        <div className='flex flex-col space-y-3'>
-            <label htmlFor="age" className='font-semibold'>Edad del artista</label>
-            <input type="text" placeholder='ej. 21' id='age' className='p-2 rounded-md bg-[#470d0db4]' value={age} onChange={e => setAge(e.target.value)}/>
-        </div>
-        <div className='flex flex-col space-y-3'>
-            <label htmlFor="band" className='font-semibold'>Banda o bandas de las que forma parte</label>
-            <input type="text" placeholder='ej. Cannibal Corpse' id='band' className='p-2 rounded-md bg-[#470d0db4]' value={band} onChange={e => setBand(e.target.value)}/>
-        </div>
-        <div className='flex flex-col space-y-3'>
-            <label htmlFor="genre" className='font-semibold'>Genero o generos musicales</label>
-            <input type="text" placeholder='ej. Pop, Rock, Jazz' id='genre' className='p-2 rounded-md resize-none bg-[#470d0db4]' value={genre} onChange={e => setGenre(e.target.value)}/>
-        </div>
-        <div className='flex justify-center'>
-            <input type="submit" value="Registrar" className='text-white bg-[#470d0de7] mt-2 p-2 rounded-md cursor-pointer' onClick={handleSubmit}/>
-        </div>
+    <form className='w-[50%] min-w-[60%] mt-14 space-y-5'>
+      <h2 className='text-center text-[#333]'>Registra a un artista!</h2>
+      <div className='border-dotted border-b-2 border-[#ccc]'></div>
+      <div className='flex flex-col space-y-3'>
+          <label htmlFor="name" className='text-[#333]'>Nombre del artista</label>
+          <input type="text" placeholder='ej. Hannah Montana' id='name' className='p-2 rounded-md border border-[#ccc]' value={name} onChange={e => setName(e.target.value)}/>
+      </div>
+      <div className='flex flex-col space-y-3'>
+          <label htmlFor="age" className='text-[#333]'>Edad del artista</label>
+          <input type="text" placeholder='ej. 21' id='age' className='p-2 rounded-md border border-[#ccc]' value={age} onChange={e => setAge(e.target.value)}/>
+      </div>
+      <div className='flex flex-col space-y-3'>
+          <label htmlFor="band" className='text-[#333]'>Banda o bandas de las que forma parte</label>
+          <input type="text" placeholder='ej. Cannibal Corpse' id='band' className='p-2 rounded-md border border-[#ccc]' value={band} onChange={e => setBand(e.target.value)}/>
+      </div>
+      <div className='flex flex-col space-y-3'>
+          <label htmlFor="genre" className='text-[#333]'>Genero o generos musicales</label>
+          <input type="text" placeholder='ej. Pop, Rock, Jazz' id='genre' className='p-2 rounded-md border border-[#ccc]' value={genre} onChange={e => setGenre(e.target.value)}/>
+      </div>
+      <div className='flex justify-center'>
+          <input type="submit" value="Registrar" className='font-bold text-[#fff] bg-[#00A1C6] mt-2 p-2 rounded-md cursor-pointer' onClick={handleSubmit}/>
+      </div>
     </form>
   </>
 }
